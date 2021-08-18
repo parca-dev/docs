25e4fd41e1ca5a90897daf78eec944b84b0134f8
Frederic Branczyk <fbranczyk@gmail.com>
2021-08-18 16:44:21 +0200

    Add simple favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
