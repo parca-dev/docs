7c698832fba846315d38b0067292aaf2f6f84563
Kemal Akkoyun <kakkoyun@gmail.com>
2021-09-28 08:41:09 +0200

    Update manifest links
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- src/components/HomepageQuickstart.js
+++ src/components/HomepageQuickstart.js
@@ -16,7 +16,7 @@ export default function HomepageQuickstart() {
         "agent": `curl -sL https://github.com/parca-dev/parca-agent/releases/download/${versions.agent}/parca-agent_${versions.agent}_\`uname -s\`_\`uname -m\`.tar.gz | tar xvfz\n./parca-agent`
     }[binaryMode]
 
-    const minikubeInstructions = `# Minikube needs to be configured with a real virtual machine driver\nminikube start --driver=virtualbox\n# Use to deploy Parca Server (API and UI)\nkubectl apply -f https://raw.githubusercontent.com/parca-dev/parca/${versions.server}/deploy/server-manifest.yaml\n# Use to deploy Parca Agent for all nodes\nkubectl apply -f https://raw.githubusercontent.com/parca-dev/parca-agent/${versions.agent}/deploy/manifest.yaml`
+    const minikubeInstructions = `# Minikube needs to be configured with a real virtual machine driver\nminikube start --driver=virtualbox\n# Use to deploy Parca Server (API and UI)\nkubectl apply -f https://raw.githubusercontent.com/parca-dev/parca/${versions.server}/deploy/manifest.yaml\n# Use to deploy Parca Agent for all nodes\nkubectl apply -f https://raw.githubusercontent.com/parca-dev/parca-agent/${versions.agent}/deploy/manifest.yaml`
     const snippet = mode == 'binary' ? curlInstructions : minikubeInstructions;
     const link = mode == 'binary' ? '/docs/binary' : '/docs/kubernetes';
     const text = mode == 'binary' ? 'Parca from Binary - Tutorial 5min ⏱️' : 'Parca in Kubernetes - Tutorial 5min ⏱️';
