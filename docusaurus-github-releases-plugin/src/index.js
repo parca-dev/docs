9586ba479acb5f42e29da1845a6d931c6b83c4b0
Kemal Akkoyun <kakkoyun@users.noreply.github.com>
2021-09-29 14:00:29 +0200

    Update Kubernetes docs (#17)
    * Add Kubernetes and OpenShift tutorials
    
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>
    
    * Update links
    
    Fix typo
    
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>
    
    * Clean TODOs
    
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- docusaurus-github-releases-plugin/src/index.js
+++ docusaurus-github-releases-plugin/src/index.js
@@ -6,11 +6,11 @@ module.exports = function () {
         async loadContent() {
             var response = await fetch("https://api.github.com/repos/parca-dev/parca/releases?per_page=1");
             var releases = await response.json();
-            const server = releases[0] ? releases[0].tag_name : 'v0.0.3-alpha.1';
+            const server = releases[0] ? releases[0].tag_name : 'v0.0.3-alpha.3';
 
             response = await fetch("https://api.github.com/repos/parca-dev/parca-agent/releases?per_page=1");
             releases = await response.json();
-            const agent = releases[0] ? releases[0].tag_name : 'v0.0.1-alpha.3';
+            const agent = releases[0] ? releases[0].tag_name : 'v0.0.1-alpha.5';
 
             return {server: server, agent: agent};
         },
