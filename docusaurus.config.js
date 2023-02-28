9e0a7a7d0ff562d9d3dc9801e92085b6cab5cc50
Matthias Loibl <mail@matthiasloibl.com>
2023-02-28 12:37:12 +0100

    Add Download button to header
    I see that this is the same as Tutorial, however, recently I struggled
    to find how to download Parca and certainly didn't expect it under
    Tutorial. Therefore, adding somewhat of duplication.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,139 +1,153 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Parca',
-  tagline: 'Open Source Infrastructure-wide continuous profiling',
-  url: 'https://parca.dev',
-  baseUrl: '/docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.svg',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  scripts: [{src: 'https://plausible.io/js/plausible.js', async: true, defer: true, 'data-domain': 'parca.dev'}],
+  title: "Parca",
+  tagline: "Open Source Infrastructure-wide continuous profiling",
+  url: "https://parca.dev",
+  baseUrl: "/docs/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.svg",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+  scripts: [
+    {
+      src: "https://plausible.io/js/plausible.js",
+      async: true,
+      defer: true,
+      "data-domain": "parca.dev",
+    },
+  ],
   plugins: [
     require.resolve("./docusaurus-github-releases-plugin/src/index.js"),
   ],
   themeConfig: {
     announcementBar: {
-      id: 'github_star',
-      content: '★ <a href="https://github.com/parca-dev/parca">If you like Parca, give it a star on GitHub!</a> ★',
-      backgroundColor: '#64b5f6', // Defaults to `#fff`.
-      textColor: '#091E42', // Defaults to `#000`.
+      id: "github_star",
+      content:
+        '★ <a href="https://github.com/parca-dev/parca">If you like Parca, give it a star on GitHub!</a> ★',
+      backgroundColor: "#64b5f6", // Defaults to `#fff`.
+      textColor: "#091E42", // Defaults to `#000`.
       isCloseable: true, // Defaults to `true`.
     },
-    metadata: [{name: 'og:image', content: 'https://parca.dev/img/logo.png'}],
+    metadata: [{ name: "og:image", content: "https://parca.dev/img/logo.png" }],
     colorMode: {
       disableSwitch: true,
     },
     navbar: {
       logo: {
-        alt: 'Parca Logo',
-        src: 'img/logo.svg',
-        href: 'https://parca.dev/',
-        target: '_self',
+        alt: "Parca Logo",
+        src: "img/logo.svg",
+        href: "https://parca.dev/",
+        target: "_self",
       },
       items: [
         {
-         type: 'doc',
-         docId: 'overview',
-         position: 'left',
-         label: 'Documentation',
+          type: "doc",
+          docId: "binary",
+          position: "left",
+          label: "Download",
         },
         {
-          type: 'doc',
-          docId: 'binary',
-          position: 'left',
-          label: 'Tutorial',
-         },
+          type: "doc",
+          docId: "overview",
+          position: "left",
+          label: "Documentation",
+        },
+        {
+          type: "doc",
+          docId: "binary",
+          position: "left",
+          label: "Tutorial",
+        },
         // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/parca-dev/parca',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/parca-dev/parca",
+          label: "GitHub",
+          position: "right",
         },
         {
-          href: 'https://twitter.com/ParcaDev',
-          label: 'Twitter',
-          position: 'right',
+          href: "https://twitter.com/ParcaDev",
+          label: "Twitter",
+          position: "right",
         },
         {
-          href: 'https://discord.gg/ZgUpYgpzXy',
-          label: 'Discord',
-          position: 'right',
-	},
+          href: "https://discord.gg/ZgUpYgpzXy",
+          label: "Discord",
+          position: "right",
+        },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Overview',
-              to: '/docs/overview',
+              label: "Overview",
+              to: "/docs/overview",
             },
             {
-              label: 'FAQ',
-              to: '/docs/faq',
+              label: "FAQ",
+              to: "/docs/faq",
             },
             {
-              label: 'Governance',
-              to: '/docs/governance',
+              label: "Governance",
+              to: "/docs/governance",
             },
           ],
         },
         {
-          title: 'Quick Start',
+          title: "Quick Start",
           items: [
             {
-              label: 'Parca from Binary',
-              to: '/docs/binary',
+              label: "Parca from Binary",
+              to: "/docs/binary",
             },
             {
-              label: 'Parca from Snapcraft',
-              to: '/docs/snap',
+              label: "Parca from Snapcraft",
+              to: "/docs/snap",
             },
             {
-              label: 'Parca in Kubernetes',
-              to: '/docs/kubernetes',
+              label: "Parca in Kubernetes",
+              to: "/docs/kubernetes",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             //{
             //  label: 'Stack Overflow',
             //  href: 'https://stackoverflow.com/questions/tagged/docusaurus',
             //},
             {
-              label: 'Discord',
-              href: 'https://discord.gg/ZgUpYgpzXy',
+              label: "Discord",
+              href: "https://discord.gg/ZgUpYgpzXy",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/ParcaDev',
+              label: "Twitter",
+              href: "https://twitter.com/ParcaDev",
             },
             {
-              label: 'YouTube',
-              href: 'https://www.youtube.com/channel/UCRrqAGuajRqIX_E_arNnYCw',
+              label: "YouTube",
+              href: "https://www.youtube.com/channel/UCRrqAGuajRqIX_E_arNnYCw",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             //{
             //  label: 'Blog',
             //  to: '/blog',
             //},
             {
-              label: 'GitHub',
-              href: 'https://github.com/parca-dev/parca',
+              label: "GitHub",
+              href: "https://github.com/parca-dev/parca",
             },
             {
               html: `
@@ -150,27 +164,26 @@ module.exports = {
       //theme: lightCodeTheme,
       darkTheme: darkCodeTheme,
     },
-	algolia: {
-	  apiKey: '525d17aaac0e93dea054f39af7b692bd',
-	  indexName: 'parca',
-	  appId: 'BH4D9OD16A',
-	  contextualSearch: false,
-	},
+    algolia: {
+      apiKey: "525d17aaac0e93dea054f39af7b692bd",
+      indexName: "parca",
+      appId: "BH4D9OD16A",
+      contextualSearch: false,
+    },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/parca-dev/parca.dev/edit/main/',
+          editUrl: "https://github.com/parca-dev/parca.dev/edit/main/",
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
